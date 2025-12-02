fd98bae65c351d792ac1c867f9616134156cac3b
Marcos Vargas <mvadvrock@gmail.com>
2025-12-02 18:30:50 -0400

    Se creo el archivo AuditLog.java en la carpeta model y el archivo ProductRepository.java en la carpeta repository

---
+++ src/main/java/com/example/app/model/AuditLog.java
@@ -0,0 +1,91 @@
+package com.example.app.model;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "audit_logs")
+public class AuditLog {
+
+    @Id
+    private String id;
+    
+    private String action; // CRUD
+    private String entity; // Product, User, etc.
+    private String entityId;
+    private String details;
+    private String username;
+    private LocalDateTime timestamp;
+    
+    
+    public AuditLog() {
+    }
+    
+    // parámetros
+    public AuditLog(String action, String entity, String entityId, String details, String username) {
+        this.action = action;
+        this.entity = entity;
+        this.entityId = entityId;
+        this.details = details;
+        this.username = username;
+        this.timestamp = LocalDateTime.now();
+    }
+    
+    // Getters y Setters
+    public String getId() {
+        return id;
+    }
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public String getAction() {
+        return action;
+    }
+    
+    public void setAction(String action) {
+        this.action = action;
+    }
+    
+    public String getEntity() {
+        return entity;
+    }
+    
+    public void setEntity(String entity) {
+        this.entity = entity;
+    }
+    
+    public String getEntityId() {
+        return entityId;
+    }
+    
+    public void setEntityId(String entityId) {
+        this.entityId = entityId;
+    }
+    
+    public String getDetails() {
+        return details;
+    }
+    
+    public void setDetails(String details) {
+        this.details = details;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+    
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}
